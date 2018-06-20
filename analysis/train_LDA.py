62b7f2e0a8c04d2b9b9fd593392cebbea22d7cbe
Giulio Carducci <carduc.92@gmail.com>
2018-06-20 16:00:43 +0200

    train and test LDA with various configs

--- analysis/train_LDA.py
+++ analysis/train_LDA.py
@@ -1,14 +1,21 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.externals import joblib
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import sys
+import re
+
+if len(sys.argv) < 2:
+    print("Usage:",sys.argv[0],"<number of topics or list of topics>")
+    sys.exit(1)
+elif len(sys.argv) == 2:
+    n_components = [int(sys.argv[1])]
+else:
+    n_components = list([int(sys.argv[i]) for i in range(1,len(sys.argv))])
 
 fname = "../data/tesi_US/US_PhD_dissertations.xlsx"
-n_components = 20
-n_features = 5000
-n_top_words = 40
-n_topics = [20, 30, 40, 50, 60, 70, 80, 90, 100]
+n_features = [7500, 10000]
 
 def get_top_n_words(corpus, n=None):
     vec = CountVectorizer().fit(corpus)
@@ -21,23 +28,6 @@ def get_top_n_words(corpus, n=None):
 def feed_data(input_data):
     for index,row in input_data.iterrows():
         yield row[' Abstract ']
-
-
-def print_top_words(model, feature_names, n_top_words):
-    for topic_idx, topic in enumerate(model.components_):
-        print()
-        message = "Topic #%d: " % topic_idx
-        message += " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
-        print(message)
-    print("\n")
-
-def savePlot(X, y, title, path):
-    plt.clf()
-    plt.plot(X, y, 'o-')
-    plt.ylabel('perplexity')
-    plt.xlabel('number of topics')
-    plt.title(title)
-    plt.savefig(path)
 
 print("Reading data...")
 abstracts = pd.read_excel(fname, usecols=[24])
@@ -55,22 +45,23 @@ analyzer = cv.build_analyzer()
 abstracts = abstracts.applymap(lambda x:analyzer(x))
 abstracts = abstracts.applymap(lambda x: " ".join(s for s in x))
 
-print("Selecting most frequent words...")
-top_freqs = get_top_n_words(abstracts[' Abstract '], n_features)
-words = list([word for word,freq in top_freqs])
+for n_words in n_features:
+    print("Selecting most frequent",n_words ,"words...")
+    top_freqs = get_top_n_words(abstracts[' Abstract '], n_words)
+    words = list([word for word,freq in top_freqs])
 
-print("Computing term-document matrix...")
-TDmatrix = cv.fit(words)
-TDmatrix = cv.transform(feed_data(abstracts))
-#tf_feature_names = cv.get_feature_names()
-#print_top_words(lda, tf_feature_names, n_top_words)
+    print("   Computing term-document matrix...")
+    TDmatrix = cv.fit(words)
+    TDmatrix = cv.transform(feed_data(abstracts))
 
-perplexities = []
-for num in n_topics:
-    print("LDA",num)
-    lda = joblib.load("LDA_"+str(num)+"_"+str(n_features)[:-3]+"k.pkl")
-    perp = lda.perplexity(TDmatrix)/num
-    print("\tperplexity:", perp)
-    perplexities.append(perp)
+    for num in n_components:
+        print("      starting LDA with",num,"topics...")
+        lda = LatentDirichletAllocation(n_components=num,
+                                        max_iter=15,
+                                        learning_method='online',
+                                        learning_offset=30.,
+                                        random_state=0,
+                                        n_jobs=-1)
 
-savePlot(n_topics, perplexities, "plot", "plot_"+str(n_features)[:-3]+"k.png")
+        lda.fit(TDmatrix)
+        joblib.dump(lda, "LDA_"+str(num)+"_"+str(n_words)[:-3]+"k.pkl")
