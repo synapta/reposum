4477b5c1cb707e7994edc76dd0265ac6dd4b060e
Giulio Carducci <carduc.92@gmail.com>
2018-09-03 16:29:16 +0200

    minor changes

--- classification/classifier/classify.py
+++ classification/classifier/classify.py
@@ -2,10 +2,7 @@ from sklearn.externals import joblib
 import text_processing as tp
 import dataset_utils as dsu
 import pandas as pd
-
-min_df = 0.0
-max_df = 0.8
-n_features = 60000
+import numpy as np
 
 def feed_data():
     for index,row in data_text.iterrows():
@@ -13,12 +10,11 @@ def feed_data():
             yield row['argomento']+' '+row['titolo']+' '+row['abs']
 
 data = dsu.read_dataset_UK(False)
-data = data.iloc[:100]
+data = data.iloc[:1000]
 print("Number of samples:",data.count()[0])
 
-cv = tp.build_vectorizer("cv", min_df, max_df, n_features)
-analyzer = cv.build_analyzer()
 vectorizer = joblib.load("vectorizer.pkl")
+analyzer = vectorizer.build_analyzer()
 
 data_subj = pd.DataFrame(tp.lemmatize_data(data[['argomento']],"argomento"), columns=['argomento'])
 data_titles = pd.DataFrame(tp.lemmatize_data(data[['titolo']],"titolo"), columns=['titolo'])
@@ -27,6 +23,8 @@ data_abs = pd.DataFrame(tp.lemmatize_data(data[['abs']],"abs"), columns=['abs'])
 data_abs = tp.preprocess_dataframe(data_abs, analyzer)
 
 data_text = data_subj.merge(data_titles,left_index=True,right_index=True).merge(data_abs,left_index=True,right_index=True)
+
+print("data_text:",data_text.count())
 
 # transform text data into vector space
 TDmatrix = vectorizer.transform(feed_data())
@@ -40,3 +38,9 @@ acc_phil = np.mean(np.equal(res,1))
 acc_nphil = np.mean(np.equal(res,0))
 print("Philosophy:", acc_phil)
 print("No philosophy:", acc_nphil)
+
+for i in range(len(res)):
+    if res[i] == 0:
+        print(data_text.iloc[i]["titolo"])
+        print(data_text.iloc[i]["argomento"])
+        input()
